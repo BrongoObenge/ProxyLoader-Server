6f0a755c39a3796735fd86da2b9f900d0bbc97f9
Jiar <Jiarshengy@gmail.com>
2014-12-17 17:31:17 +0000

    Changed some proxies

--- RefreshProxy.py
+++ RefreshProxy.py
@@ -3,10 +3,14 @@ import urllib
 import time
 import os
 class RefreshProxy:
-    proxyList = ["46.165.249.92:80",
-                 "5.196.116.22:7808",
-                 "199.200.120.140:7808",
-                 "199.200.120.37:7808",
+    proxyList = [
+                 "62.153.96.164:80",
+                 "83.128.239.152:80",
+                 "94.210.6.125:80",
+                 "185.30.147.197:8080",
+                 "5.53.16.183:8080",
+
+
                  ]
 
     def start(self):
@@ -14,13 +18,14 @@ class RefreshProxy:
         exportString = "export http_proxy='%s'"
         x = 0
         while True:
+            print "Starting Proxy: "+ exportString % workingList[x]
             os.system(exportString % workingList[x])
             if x+1 == len(workingList):
                 x = 0
             else:
                 x+=1
             print "Loaded new proxy, sleeping 30 minutes."
-            time.sleep(1800)
+            #time.sleep(1800)
 
     def checkWorkingProxy(self, proxyList):
         workingProxy = []
@@ -28,11 +33,12 @@ class RefreshProxy:
         for x in range(len(proxyList)):
             proxy = str(proxyList[x])
             try:
-                urllib.urlopen("http://google.com", proxies={'http': "http://"+proxy})
-
+                urllib.urlopen("http://tweakers.net/pricewatch/323351/crucial-ballistix-tactical-blt2c4g3d1608et3lx0ceu/specificaties/", proxies={'http': "http://"+proxy})
             except IOError:
-                print a,"/",len(proxyList)," Proxy not working"
+                print "Proxy not working: " + proxy
                 a +=1
+            except:
+                print "Error"
             else:
                 workingProxy.append(str(proxyList[x]))
         print "Done loading proxies."
